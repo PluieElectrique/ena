805ec6d6d7b05e5aa5b4c38dd88edbd84e946a5b
PluieElectrique <41453973+PluieElectrique@users.noreply.github.com>
2018-11-18 00:55:44 -0800

    Alphabetize variants of FetchError

--- src/actors/fetcher/error.rs
+++ src/actors/fetcher/error.rs
@@ -7,35 +7,35 @@ use tokio;
 
 #[derive(Debug, Fail)]
 pub enum FetchError {
-    #[fail(display = "Hyper error: {}", _0)]
-    HyperError(hyper::Error),
-
     #[fail(display = "Bad status: {}", _0)]
     BadStatus(hyper::StatusCode),
-
-    #[fail(display = "JSON error: {}", _0)]
-    JsonError(serde_json::Error),
-
-    #[fail(display = "Resource not modified")]
-    NotModified,
-
-    #[fail(display = "Media already exists")]
-    ExistingMedia,
-
-    #[fail(display = "Resource not found: {}", _0)]
-    NotFound(String),
 
     #[fail(display = "API returned empty data")]
     EmptyData,
 
-    #[fail(display = "Timer error: {}", _0)]
-    TimerError(tokio::timer::Error),
+    #[fail(display = "Media already exists")]
+    ExistingMedia,
+
+    #[fail(display = "Hyper error: {}", _0)]
+    HyperError(hyper::Error),
 
     #[fail(display = "IO error: {}", _0)]
     IoError(std::io::Error),
 
+    #[fail(display = "JSON error: {}", _0)]
+    JsonError(serde_json::Error),
+
     #[fail(display = "Mailbox error: {}", _0)]
     MailboxError(actix::MailboxError),
+
+    #[fail(display = "Resource not found: {}", _0)]
+    NotFound(String),
+
+    #[fail(display = "Resource not modified")]
+    NotModified,
+
+    #[fail(display = "Timer error: {}", _0)]
+    TimerError(tokio::timer::Error),
 }
 
 macro_rules! impl_enum_from {
