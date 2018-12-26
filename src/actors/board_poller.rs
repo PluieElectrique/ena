091b148bd4dd2b8a75bb993ce2cb56c9c3ec7ac3
PluieElectrique <41453973+PluieElectrique@users.noreply.github.com>
2018-12-26 03:45:48 -0800

    Fully explain anchor thread heuristic

--- src/actors/board_poller.rs
+++ src/actors/board_poller.rs
@@ -81,40 +81,73 @@ impl BoardPoller {
         let mut updates = vec![];
 
         let push_removed = {
+            // To determine if a removed thread was bumped off or deleted, we use the "anchor
+            // thread" heuristic. Note that when in doubt, we always assume that a removed thread
+            // was bumped off, as deletions are much rarer.
+            //
+            // Now, the anchor thread is the thread which:
+            // 1. Is the last thread of the current thread list,
+            // Note: Any current thread which satisfies 2 and 3 is an anchor, but the last thread
+            //       makes for the best anchor, as it lets us catch as many deleted threads as
+            //       possible. To keep things simple, if the last thread turns out to not be a valid
+            //       anchor (which is unlikely), we don't consider other threads.
             let anchor_index = curr_threads.last().map(|last_thread| {
                 self.threads[&board]
                     .iter()
                     .rev()
+                    // 2. Appears in the previous thread list (i.e. is not a new thread), and
                     .find(|thread| thread.no == last_thread.no)
-                    // If a board is fast and our poll interval is long, it might be possible for a
-                    // thread to be bumped and then fall to the bottom of the board in one interval.
-                    // This would break the heuristic and could cause bumped-off threads to be
-                    // marked as deleted. So, we check last_modified to ensure that this thread
-                    // hasn't been bumped. Note that this check will also reject saged threads,
-                    // which are valid anchors. That's okay, though, because we would rather reject
-                    // some valid anchors than accept invalid ones.
+                    // 3. Has not been bumped since the last poll.
+                    // Note: last_modified is updated when a new post is added and bumps the thread.
+                    //       But it's also updated when the thread is saged. We should accept saged
+                    //       threads as anchors, since they weren't bumped, but we can't tell them
+                    //       apart from bumped threads. So, this check could reject a valid anchor.
                     .filter(|thread| thread.last_modified == last_thread.last_modified)
+                    // Get the index of the anchor in the previous thread list
                     .map(|thread| thread.bump_index)
             });
 
             move |thread: &Thread, updates: &mut Vec<ThreadUpdate>| {
                 match anchor_index {
+                    // We found an anchor and will use it to determine if a thread has been
+                    // deleted or bumped off.
                     Some(Some(anchor)) => {
-                        // We found an anchor and will use it to determine if a thread has been
-                        // deleted or bumped off.
                         if thread.bump_index < anchor {
+                            // If the removed thread was before the anchor, it was deleted.
+                            // Proof:
+                            //   1. Two threads only swap orders when the bottom one is bumped.
+                            //      (Stickying the bottom thread would also swap the order, but
+                            //      that's too rare to worry about. Stickies should also update
+                            //      last_modified, which will be caught by the check above.)
+                            //   2. The anchor was not bumped and was behind the removed thread.
+                            //   3. So, the anchor was always behind the removed thread.
+                            //   4. Only the last thread on the board can be bumped off.
+                            //   5. The removed thread could never have been the last thread on the
+                            //      board, as the anchor was always behind it.
+                            //   6. So, the removed thread could not have been bumped off.
                             updates.push(Deleted(thread.no));
                         } else {
+                            // A thread after the anchor could have been the last thread at some
+                            // point. So, we assume it was bumped off. Even if the thread lists are:
+                            //        No. 1             No. 1
+                            //  prev  No. 2  -->  curr  No. 2
+                            //        No. 3
+                            // We can't say for sure that No. 3 was deleted. Between polls, a new
+                            // "phantom" thread could have been added, bumping off No. 3, and then
+                            // deleted soon after. This could happen if our poll_interval is long or
+                            // threads are being deleted quickly (e.g. in response to a raid).
                             updates.push(BumpedOff(thread.no));
                         }
                     }
+                    // The board isn't empty, but we didn't find a valid anchor (maybe all threads
+                    // are new or were modified). So, we assume all removed threads were bumped off.
                     Some(None) => {
-                        // The board isn't empty, but we didn't find a valid anchor. Without enough
-                        // information, we assume all removed threads were bumped off.
                         updates.push(BumpedOff(thread.no));
                     }
+                    // There is no last thread in the current list. So, the board is empty and all
+                    // threads were deleted. (The "phantom thread" argument from above applies here,
+                    // but if the board is empty, it's just more likely everything was deleted.)
                     None => {
-                        // The board is empty, so all threads must have been deleted.
                         updates.push(Deleted(thread.no));
                     }
                 }
